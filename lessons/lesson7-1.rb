1dab7c2959d959ab1d17a4be498cb93c8ad77ba3
awoka333 <na23okzw.htre@gmail.com>
2020-10-21 12:04:00 +0000

    add lesson7

---
+++ lessons/lesson7-1.rb
@@ -0,0 +1,8 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+a = gets
+b = gets
+c = a.to_i * b.to_i
+puts "計算結果を出力します"
+puts "a*b=#{c}"
+puts "計算を終了します"
---
+++ lessons/lesson7-2.rb
@@ -0,0 +1,17 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+i = gets.to_i
+
+for i in 1..i do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+end
+
+puts "計算を終了します"
